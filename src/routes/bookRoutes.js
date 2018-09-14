8c2da0629f29873581af03f330b8a169749c346f
vassilis.foto <vassilis@Vassiliss-MacBook-Air.local>
2018-09-14 19:05:21 +0200

    Routes: Adding Routes in application

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,88 +1,102 @@
 const express = require('express');
-// const debug = require('debug')('myapplication-bookRouter');
 
 const bookRouter = express.Router();
 
 /**
  * Book Router definition
  */
-
-const books = [
-  {
-    title: 'In Search of Lost Time',
-    genre: '',
-    author: 'Marcel Proust',
-    read: false
-  },
-  {
-    title: 'Don Quixote',
-    genre: '',
-    author: 'Miguel de Cervantes',
-    read: false
-  },
-  {
-    title: 'Ulysses',
-    genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
-    author: 'James Joyce',
-    read: false
-  },
-  {
-    title: 'The Great Gatsby',
-    genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
-    author: 'F. Scott Fitzgerald',
-    read: false
-  },
-  {
-    title: 'Moby Dick',
-    genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
-    author: 'Herman Melville',
-    read: false
-  },
-  {
-    title: 'Hamlet',
-    genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
-    author: 'William Shakespeare',
-    read: false
-  },
-  {
-    title: 'War and Peace',
-    genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
-    author: 'Leo Tolstoy',
-    read: false
-  },
-  {
-    title: 'The Odyssey',
-    genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
-    author: 'Leoby Homer',
-    read: false
-  },
-  {
-    title: 'One Hundred Years of Solitude',
-    genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
-    author: 'Gabriel Garcia Marquez',
-    read: false
-  }
-];
-
-bookRouter.route('/')
-  .get((req, res) => res.render('books',
+function router(nav) {
+  const books = [
     {
-      books,
-      title: 'books',
-      nav: [
-        {
-          title: 'Books',
-          link: 'books'
-        },
-        {
-          title: 'Authors',
-          link: 'authors'
-        }]
-    }));
+      id: 0,
+      title: 'In Search of Lost Time',
+      genre: '',
+      author: 'Marcel Proust',
+      read: false
+    },
+    {
+      id: 1,
+      title: 'Don Quixote',
+      genre: '',
+      author: 'Miguel de Cervantes',
+      read: false
+    },
+    {
+      id: 2,
+      title: 'Ulysses',
+      genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
+      author: 'James Joyce',
+      read: false
+    },
+    {
+      id: 3,
+      title: 'The Great Gatsby',
+      genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
+      author: 'F. Scott Fitzgerald',
+      read: false
+    },
+    {
+      id: 4,
+      title: 'Moby Dick',
+      genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
+      author: 'Herman Melville',
+      read: false
+    },
+    {
+      id: 5,
+      title: 'Hamlet',
+      genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
+      author: 'William Shakespeare',
+      read: false
+    },
+    {
+      id: 6,
+      title: 'War and Peace',
+      genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
+      author: 'Leo Tolstoy',
+      read: false
+    },
+    {
+      id: 7,
+      title: 'The Odyssey',
+      genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
+      author: 'Leoby Homer',
+      read: false
+    },
+    {
+      id: 8,
+      title: 'One Hundred Years of Solitude',
+      genre: 'Anim aliquip consectetur duis consectetur nostrud cupidatat ipsum sit.',
+      author: 'Gabriel Garcia Marquez',
+      read: false
+    }
+  ];
 
-bookRouter.route('/single')
-  .get((req, res) => {
-    res.send(`Hello single book by ${process.env.employee1}`);
-  });
+  bookRouter.route('/')
+    .get((req, res) => res.render('bookListView',
+      {
+        books,
+        title: 'books',
+        nav
+      }));
 
-module.exports = bookRouter;
+  bookRouter.route('/:id')
+    .get((req, res) => {
+      const { id } = req.params;
+      // 1. query the D.B to get book by id
+      const book = books[id];
+      // 2. populate the view with dynamic data
+      res.render(
+        'bookView',
+        {
+          nav,
+          title: 'Library',
+          book
+        }
+      );
+    });
+
+  return bookRouter;
+}
+
+module.exports = router;
